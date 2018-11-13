f47630db95f10afbbce64e840a14970a3e3679ae
filipdbrsk <filip.dbrsk@gmail.com>
2018-11-13 16:37:59 +0100

    updates of the baselines

--- EntityIdentity/baselines/run_and_evaluate.sh
+++ EntityIdentity/baselines/run_and_evaluate.sh
@@ -1,3 +1,6 @@
-python3 name_students_baseline.py -p full
-cd ../data_preparation
-python3 evaluate.py -p full -s name_students_baseline
+data=( 'full' 'partial' )
+for which_data in "${data[@]}" ; do
+	#python3 name_students_baseline.py -p full
+	python3 noclash_baseline.py -p $which_data
+	python3 ../data_preparation/evaluate.py -p $which_data -s name_students_baseline
+done
