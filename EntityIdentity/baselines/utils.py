5aae27cfd03f465bee395df186b3e378d912ad5a
filipdbrsk <filip.dbrsk@gmail.com>
2018-11-19 17:54:19 +0100

    optimizing the extractors

--- EntityIdentity/baselines/utils.py
+++ EntityIdentity/baselines/utils.py
@@ -4,6 +4,12 @@ from collections import defaultdict
 
 ########## FUNCTIONS RELATED TO COREFERENCE PROCESSING #########################
 
+def get_sentence_offsets(doc):
+    sent_offsets=[]
+    for s in doc.sents:
+        sent_offsets.append(tuple([s.start_char, s.end_char]))
+    return sent_offsets
+
 def stringify_cluster_mentions(mentions):
     """Prepare a list of text values for a cluster, based on a list of mention objects."""
     stringy=[]
@@ -19,14 +25,15 @@ def get_people_spans(names, text):
         spans[name]=oc
     return spans
 
-def get_coref_spans(names, text, nlp):
+def get_coref_spans(names, text, doc):
     """Process the file with coreference relations."""
-    doc = nlp(text)
     
-    spans=get_people_spans(names, text)
+    people_spans=get_people_spans(names, text)
     
+    spans=defaultdict(list)
+
     if not doc._.has_coref:
-        return spans, None
+        return people_spans, spans, None
 
     clusters=doc._.coref_clusters
     for c in clusters:
@@ -35,13 +42,31 @@ def get_coref_spans(names, text, nlp):
             if person_name in mentions:
                 for m in c.mentions:
                     oc=[m.start_char, m.end_char]
-                    if oc not in spans[person_name]:
+                    if oc not in spans[person_name] and oc not in people_spans[person_name]:
                         spans[person_name].append(oc)
-    return spans, clusters
+    return people_spans, spans, clusters
 
 ################### OTHER HELPER FUNCTIONS ############################
 
-def find_closest_person(attr_span, people_spans, min_dist=10):
+def get_sentence(span, sentence_offsets):
+    for i, offset in enumerate(sentence_offsets):
+        if span<offset[0]:
+            break
+    return i
+
+def check_if_best_option(ps, attr_span, min_dist, closest_person, sentence_offsets, name):
+    if get_sentence(ps[0], sentence_offsets)!=get_sentence(attr_span[0], sentence_offsets):
+        return min_dist, closest_person
+    if ps[0]<attr_span[0]: # person mentioned before the attribute
+        dist=attr_span[0]-ps[1]
+    else: # person mentioned after the attribute
+        dist=ps[0]-attr_span[1]
+    if dist<min_dist:
+        min_dist=dist
+        closest_person=name
+    return min_dist, closest_person
+
+def find_closest_person(attr_span, people_spans, coref_spans, sentence_offsets, min_dist=10):
     """Find the closest person to an attribute value."""
     closest_person=None
     for name, person_spans in people_spans.items():
@@ -49,13 +74,10 @@ def find_closest_person(attr_span, people_spans, min_dist=10):
             # make sure to exclude cases where the attribute and the name overlap
             if (ps[0]<=attr_span[0] and ps[1]>=attr_span[1]) or (attr_span[0]<=ps[0] and attr_span[1]>=ps[1]):
                 continue
-            if ps[0]<attr_span[0]: # person mentioned before the attribute
-                dist=attr_span[0]-ps[1]
-            else: # person mentioned after the attribute
-                dist=ps[0]-attr_span[1]
-            if dist<min_dist:
-                min_dist=dist
-                closest_person=name
+            min_dist, closest_person=check_if_best_option(ps, attr_span, min_dist, closest_person, sentence_offsets, name)
+    for name, c_spans in coref_spans.items():
+        for cs in c_spans:
+            min_dist, closest_person=check_if_best_option(cs, attr_span, min_dist, closest_person, sentence_offsets, name)
     return closest_person, min_dist
 
 def get_closest_value_per_person(pairs):
@@ -79,7 +101,7 @@ def singularize_data(extractors_json):
     result=defaultdict(dict)
     for attribute, extracted in extractors_json.items():
         for name, value in extracted.items():
-            result[name][attribute]=value
+            result[name][attribute]=value.lower()
     return dict(result)
 
 def get_participant_info(data, sections=['shooter-section', 'victim-section']):
