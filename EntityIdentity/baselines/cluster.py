59a398a3c28e70e8a9b86e2905ec3f48b3ab9520
filipdbrsk <filip.dbrsk@gmail.com>
2018-12-12 13:13:32 +0100

    baselines improvement of the experimental setup

--- EntityIdentity/baselines/cluster.py
+++ EntityIdentity/baselines/cluster.py
@@ -78,40 +78,47 @@ def count_values(data):
 
 if __name__ == "__main__":
 	# Parse arguments
-	parser = argparse.ArgumentParser(description='Simple baseline based on people\'s full names.')
+	parser = argparse.ArgumentParser(description='Run clustering based on extracted properties.')
+
 	parser.add_argument('-p', '--partition', required=True, type=str,
 		help='On which data partition to run it: full or partial')
-    
 	parser.add_argument('-e', '--extractor', required=True, type=str,
                 help='Which extractor to use: gold or auto')
 	parser.add_argument('-m', '--merger', required=True, type=str,
                 help='Which merger to use: exact or noclash')
-    
+	parser.add_argument('-c', '--combination', required=True, type=str,
+                help='Which combination of properties: p0 (name only) - p13 (all properties)')
+
 	args = vars(parser.parse_args())
 	which_partition=args['partition']
 	which_extractor=args['extractor']
 	merger=args['merger']
+	which_combination=args['combination']
 
 	# set directories
 	if which_extractor=='gold':
             input_dir='../data/input/%s/annotation' % which_partition
-            output_dir='../data/system/name_students_baseline/%s' % which_partition
 	else:
             input_dir='extracted_data/%s' % which_partition
-            output_dir='../data/system/auto_ext_baseline/%s' % which_partition
-        
+	output_dir='../data/system/%s/%s/%s/%s' % (which_extractor, which_partition, merger, which_combination)
+
 	# Define which properties to consider
-#	properties=['Name']
-	properties=[]
-#	properties+=['CauseOfDeath', 'EducationLevel', 'Residence']
-#	properties+=['Residence', 'Religion', 'Ethnicity']
-	properties+=['CauseOfDeath', 'EducationLevel', 'Residence', 'Religion', 'Ethnicity', 'PastConviction' , 'BirthPlace'] # 
-	properties+=['Gender', 'Age', 'DeathDate', 'DeathPlace']
-#	properties+=['Age', 'Gender']
-#	properties+=['Age']
+	prop_combos = {'p0': ['Name'], 'p1': ['Name', 'CauseOfDeath', 'EducationLevel'], 'p2': ['Name', 'CauseOfDeath', 'EducationLevel', 'Residence'],
+                       'p3': ['Name', 'CauseOfDeath', 'EducationLevel', 'Residence', 'Religion', 'Ethnicity', 'PastConviction'],
+                       'p4': ['Name', 'CauseOfDeath', 'EducationLevel', 'Residence', 'Religion', 'Ethnicity', 'PastConviction', 'BirthPlace'],
+                       'p5': ['Name', 'Age'],
+                       'p6': ['Name', 'Age', 'Gender'],
+                       'p7': ['Name', 'Age', 'Gender', 'DeathDate'],
+                       'p8': ['Name', 'Age', 'Gender', 'DeathPlace'],
+                       'p9': ['Name', 'Age', 'Gender', 'DeathDate', 'DeathPlace'],
+                       'p10': ['Name', 'AgeGroup', 'Gender', 'DeathDate'],
+                       'p11': ['Name', 'AgeGroup', 'Gender', 'DeathPlace'],
+                       'p12': ['Name', 'AgeGroup', 'Gender', 'DeathDate', 'DeathPlace'],
+                       'p13': ['Name', 'CauseOfDeath', 'EducationLevel', 'Residence', 'Religion', 'Ethnicity', 'PastConviction', 'BirthPlace', 'Age', 'AgeGroup', 'Gender', 'DeathDate', 'DeathPlace']
+                      }
+	properties=prop_combos[which_combination]
 
 	for input_file in glob.glob('%s/*' % input_dir):
-		print(input_file)
 		with open(input_file, 'rb') as f:
 			group_by_name_plus=defaultdict(set)
 			input_data=pickle.load(f)
@@ -131,6 +138,5 @@ if __name__ == "__main__":
 			system_json=transform_to_json(new_data)
                     
 		output_file='%s/%s.json' % (output_dir, (input_file.split('/')[-1]).split('.')[0])
-		print(output_file)
 		with open(output_file, 'w') as w:
 			json.dump(system_json, w)
--- EntityIdentity/baselines/run_and_evaluate.sh
+++ EntityIdentity/baselines/run_and_evaluate.sh
@@ -1,12 +1,22 @@
 data=( 'full' 'partial' )
-extraction='auto'
+#extraction='auto'
+extraction='gold'
 combine=( 'exact' 'noclash' )
+props=( 'p0' 'p1' 'p2' 'p3' 'p4' 'p5' 'p6' 'p7' 'p8' 'p9' 'p10' 'p11' 'p12' 'p13' )
+
+#baseline="auto_ext_baseline"
+baseline="name_students_baseline"
 
 for cmb in "${combine[@]}" ; do
 	for which_data in "${data[@]}" ; do
-		echo "$which_data $cmb"
-		python3 cluster.py -p $which_data -e $extraction -m $cmb
-        
-		python3 ../data_preparation/evaluate.py -p $which_data -s auto_ext_baseline
+		for which_props in "${props[@]}"; do
+			echo "\n$extraction $which_data $cmb $which_props"
+			system_dir="${extraction}/${which_data}/${cmb}/${which_props}"
+			mkdir -p "../data/system/${system_dir}"
+
+
+			python3 cluster.py -p $which_data -e $extraction -m $cmb -c $which_props
+			python3 ../data_preparation/evaluate.py -p $which_data -s $system_dir
+		done
 	done
 done
