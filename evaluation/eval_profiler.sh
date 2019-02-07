4161b05127f6bb923af9d50558350b8cebb08de7
filipdbrsk <filip.dbrsk@gmail.com>
2019-02-07 16:49:55 +0100

    running evaluation of profiling

--- evaluation/eval_profiler.sh
+++ evaluation/eval_profiler.sh
@@ -1,6 +1,6 @@
 extractors=( 'auto' 'gold' ) 
 
 for extractor in "${extractors[@]}" ; do
-	python evaluate.py -s "${extractor}_profiling/partial" -p partial
-        #python evaluate.py -s "${extractor}_profiling/full" -p full
+	#python evaluate.py -s "${extractor}_profiling/partial" -p partial
+        python evaluate.py -s "${extractor}_profiling/full" -p full
 done
